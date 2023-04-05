03af08468b9afab7fc3e0ff767f2567f5b7998f5
JvFontenele <jvfont80@gmail.com>
2023-04-05 14:38:35 -0300

    secao 4

--- src/components/Sections/Section3.vue
+++ src/components/Sections/Section3.vue
@@ -17,7 +17,16 @@
               </p>
             </div>
           </div>
-
+          <div class="cardIcons">
+            <img src="../../assets/secao3/vinho.svg" alt="" />
+            <div>
+              <p class="titulo_cardIcons">Reservas</p>
+              <p>
+                É muito facíl reservar um espaço<br />
+                com LIVO, basta dois cliques.
+              </p>
+            </div>
+          </div>
           <div class="cardIcons">
             <img src="../../assets/secao3/carregar.svg" alt="" />
             <div>
@@ -25,6 +34,16 @@
               <p>
                 Convites liberados para visita recorrente. <br />
                 Você escolhe o dia e a hora.
+              </p>
+            </div>
+          </div>
+          <div class="cardIcons">
+            <img src="../../assets/secao3/moto.svg" alt="" />
+            <div>
+              <p class="titulo_cardIcons">Delivery</p>
+              <p>
+                Adiante sua portaria avisando que você <br />
+                aguarda delivery ou algum motorista.
               </p>
             </div>
           </div>
@@ -39,22 +58,96 @@
             </div>
           </div>
           <div class="cardIcons">
-            <img src="../../assets/secao3/carregar.svg" alt="" />
+            <img src="../../assets/secao3/jornal.svg" alt="" />
             <div>
-              <p class="titulo_cardIcons">Recorrente</p>
+              <p class="titulo_cardIcons">Mural</p>
               <p>
-                Convites liberados para visita recorrente. <br />
-                Você escolhe o dia e a hora.
+                Informações compartilhadas. Sobre<br />
+                qualquer tipo de evento no condomínio.
               </p>
             </div>
           </div>
           <div class="cardIcons">
-            <img src="../../assets/secao3/carregar.svg" alt="" />
+            <img src="../../assets/secao3/calendario.svg" alt="" />
             <div>
-              <p class="titulo_cardIcons">Recorrente</p>
+              <p class="titulo_cardIcons">Eventos</p>
               <p>
-                Convites liberados para visita recorrente. <br />Você escolhe o
-                dia e a hora.
+                Múltiplos convites para substituir<br />
+                a clássica lista da festa.
+              </p>
+            </div>
+          </div>
+          <div class="cardIcons">
+            <img src="../../assets/secao3/patinha.svg" alt="" />
+            <div>
+              <p class="titulo_cardIcons">PET</p>
+              <p>Seu pet com cadastro.</p>
+            </div>
+          </div>
+          <div class="cardIcons">
+            <img src="../../assets/secao3/placa.svg" alt="" />
+            <div>
+              <p class="titulo_cardIcons">Ocorrências e Enquetes</p>
+              <p>
+                Em tempo real perceba a temperatura<br />
+                de intenções. No WhatsApp tudo<br />
+                se perde... Organize melhor o que<br />
+                o condomínio precisa.<br />
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="boxBtn">
+          <img
+            class="botao"
+            src="../../assets/secao3/botaoparabaixo.svg"
+            alt=""
+          />
+        </div>
+        <div class="box-card">
+          <div class="card">
+            <img src="../../assets/secao3/placa.svg" alt="" />
+            <div>
+              <p class="titulo_card">App do proprietário</p>
+              <p>
+                Residencial e comercial. <br />
+                Livo é intuitivo e simples de usar, igual <br />
+                pedir comida no ifood ou mandar um <br />
+                arquivo pelo WhatsApp. <br />
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="../../assets/secao3/placa.svg" alt="" />
+            <div>
+              <p class="titulo_card">Visitante e prestador</p>
+              <p>
+                Cadastro rápido e seguro seguindo<br />
+                nossa LGPD.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="../../assets/secao3/placa.svg" alt="" />
+            <div>
+              <p class="titulo_card">LIVO web</p>
+              <p>
+                Prático para quem usa no dia a dia,<br />
+                para as equipes nas portarias<br />
+                acessarem sem muitos cliques ou<br />
+                várias telas.<br />
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="../../assets/secao3/placa.svg" alt="" />
+            <div>
+              <p class="titulo_card">LIVO relatórios</p>
+              <p>
+                Para gerentes e síndicos<br />
+                acompanharem a estatística geral<br />
+                por meio de dados. Formato Power BI.<br />
               </p>
             </div>
           </div>
@@ -95,7 +188,7 @@
 
 .conteudo {
   position: absolute;
-  top: -50px;
+  top: -40px;
   width: 100%;
   height: 100%;
   z-index: 2;
@@ -117,20 +210,33 @@ h1 {
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 25vw;
+  height: 20vw;
+}
+h2 {
+  font-size: 4vw;
+  line-height: 1.2em;
+}
+p {
+  margin: 25px 0;
+  font-size: 2vw;
+  font-weight: 200;
+  padding: 0;
 }
 
 .boxIcones {
   /* background: red; */
   width: 100%;
-  height: 500px;
+  height: 40vw;
   padding: 0 12vw;
   top: -80px;
+  /* background: red; */
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
 }
 
 .cardIcons {
-  /* background: red; */
-  width: 50%;
+  width: 100%;
   display: flex;
   margin-top: 15px;
   height: 7vw;
@@ -153,24 +259,13 @@ h1 {
   line-height: 1.2em;
 }
 .titulo_cardIcons {
-  font-weight: 600 !important;
-  font-size: 1.6vw !important;
+  font-weight: 400 !important;
+  font-size: 1.9vw !important;
   margin-bottom: 5px !important;
 }
 
 .box-descricao {
   padding: 0 20em;
-}
-
-h2 {
-  font-size: 4vw;
-  line-height: 1.2em;
-}
-p {
-  margin: 25px 0;
-  font-size: 2vw;
-  font-weight: 200;
-  padding: 0;
 }
 
 .btn-conteudo {
@@ -188,6 +283,49 @@ p {
   margin-left: 25px;
   margin-right: 5px;
   font-weight: 600;
+}
+.boxBtn {
+  /* background: rebeccapurple; */
+  text-align: center;
+}
+.botao {
+  width: 7vw;
+}
+
+.box-card {
+  /* background: red; */
+  width: 100%;
+  height: 20vw;
+  display: flex;
+  padding: 10vw;
+  margin-top: 5vw;
+  justify-content: space-around;
+  align-items: center;
+}
+.card {
+  background: rgba(202, 110, 237, 0.6);
+  height: 20vw;
+  width: 15vw;
+  border-radius: 20px;
+  text-align: center;
+  padding: 20px;
+}
+
+.card div {
+  text-align: start;
+}
+.card p {
+  font-size: 0.79vw;
+  margin: 0;
+}
+.titulo_card {
+  font-weight: 500;
+  font-size: 1.2vw !important;
+  margin: 0 0px 10px 0px !important;
+}
+.card img {
+  width: 80px;
+  margin: 1.4vw 0 3vw 0;
 }
 
 @media screen and (max-width: 600px) {
